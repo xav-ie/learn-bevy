726c16352c55a9e70bbfc82c1eeeba65f342c216
xav-ie <github@xav.ie>
2025-03-15 10:36:05 -0700

    feat(spawn_stars_over_time): add
    Use the timer we created in a query and create new stars over time

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -35,6 +35,7 @@ fn main() {
         .add_system(player_hit_star)
         .add_system(update_score)
         .add_system(tick_spawn_star_timer)
+        .add_system(spawn_stars_over_time)
         .run();
 }
 
@@ -360,4 +361,25 @@ pub fn update_score(score: Res<Score>) {
 
 pub fn tick_spawn_star_timer(mut spawn_star_timer: ResMut<SpawnStarTimer>, time: Res<Time>) {
     spawn_star_timer.timer.tick(time.delta());
+}
+
+pub fn spawn_stars_over_time(
+    mut commands: Commands,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
+    spawn_star_timer: Res<SpawnStarTimer>,
+) {
+    if spawn_star_timer.timer.finished() {
+        let window = window_query.get_single().unwrap();
+        let random_x = random::<f32>() * (window.width() - STAR_SIZE) + STAR_SIZE / 2.0;
+        let random_y = random::<f32>() * (window.height() - STAR_SIZE) + STAR_SIZE / 2.0;
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(random_x, random_y, 0.0),
+                texture: asset_server.load("sprites/star.png"),
+                ..default()
+            },
+            Star {},
+        ));
+    }
 }
