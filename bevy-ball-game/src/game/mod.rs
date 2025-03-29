46f97eed4b06fef9f9dbbd02976e9a29af2e49ad
xav-ie <github@xav.ie>
2025-03-29 11:41:48 -0400

    feat(game): add SimulationState

--- bevy-ball-game/src/game/mod.rs
+++ bevy-ball-game/src/game/mod.rs
@@ -2,12 +2,15 @@ pub mod enemy;
 pub mod player;
 pub mod score;
 pub mod star;
+mod systems;
+
 use enemy::EnemyPlugin;
 use player::PlayerPlugin;
 use score::ScorePlugin;
 use star::StarPlugin;
+use systems::*;
 
-use crate::events::GameOver;
+use crate::{events::GameOver, AppState};
 
 use bevy::prelude::*;
 
@@ -15,10 +18,19 @@ pub struct GamePlugin;
 
 impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.add_event::<GameOver>()
+        app.add_state::<SimulationState>()
+            .add_event::<GameOver>()
             .add_plugin(PlayerPlugin)
             .add_plugin(StarPlugin)
             .add_plugin(EnemyPlugin)
-            .add_plugin(ScorePlugin);
+            .add_plugin(ScorePlugin)
+            .add_system(toggle_simulation.run_if(in_state(AppState::Game)));
     }
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum SimulationState {
+    Running,
+    #[default]
+    Paused,
 }
