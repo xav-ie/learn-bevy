9b19ea7027af258377e9680bdcf198e1d6bee418
xav-ie <github@xav.ie>
2025-03-30 22:57:23 -0400

    feat(transitions): add

--- bevy-ball-game/src/main.rs
+++ bevy-ball-game/src/main.rs
@@ -16,6 +16,8 @@ fn main() {
         .add_plugin(GamePlugin)
         .add_plugin(MainMenuPlugin)
         .add_startup_system(spawn_camera)
+        .add_system(transition_to_game_state)
+        .add_system(transition_to_main_menu_state)
         .add_system(update_camera_on_resize)
         .add_system(exit_game)
         .add_system(handle_game_over)
--- bevy-ball-game/src/systems.rs
+++ bevy-ball-game/src/systems.rs
@@ -1,5 +1,5 @@
 // use crate::components::*;
-use crate::events::*;
+use crate::{events::*, AppState};
 // use crate::resources::*;
 use bevy::app::AppExit;
 use bevy::{
@@ -29,6 +29,30 @@ pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<Pr
         transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
         ..default()
     });
+}
+
+pub fn transition_to_game_state(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::G) {
+        if app_state.0 != AppState::Game {
+            commands.insert_resource(NextState(Some(AppState::Game)));
+        }
+    }
+}
+
+pub fn transition_to_main_menu_state(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    app_state: Res<State<AppState>>,
+) {
+    if keyboard_input.just_pressed(KeyCode::M) {
+        if app_state.0 != AppState::MainMenu {
+            commands.insert_resource(NextState(Some(AppState::MainMenu)));
+        }
+    }
 }
 
 pub fn exit_game(
