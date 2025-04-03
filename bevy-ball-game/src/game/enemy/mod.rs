27ad36d25ec55761c66241eed5b80fd1a655d5fc
xav-ie <github@xav.ie>
2025-04-02 23:18:25 -0400

    feat(enemy): pause systems when game is paused

--- bevy-ball-game/src/game/enemy/mod.rs
+++ bevy-ball-game/src/game/enemy/mod.rs
@@ -3,6 +3,8 @@ use resources::SpawnEnemyTimer;
 use systems::*;
 
 use crate::AppState;
+
+use super::SimulationState;
 pub mod components;
 pub mod resources;
 mod systems;
@@ -15,12 +17,18 @@ pub struct EnemyPlugin;
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
         app.init_resource::<SpawnEnemyTimer>()
-            .add_system(enemy_movement)
-            .add_system(update_enemy_direction.after(enemy_movement))
-            .add_system(confine_enemy_movement.after(enemy_movement))
-            .add_system(tick_spawn_enemy_timer)
-            .add_system(spawn_enemies_over_time);
             .add_system(spawn_enemies.in_schedule(OnEnter(AppState::Game)))
             .add_system(despawn_enemies.in_schedule(OnExit(AppState::Game)))
+            .add_systems(
+                (
+                    enemy_movement,
+                    update_enemy_direction.after(enemy_movement),
+                    confine_enemy_movement.after(enemy_movement),
+                    tick_spawn_enemy_timer,
+                    spawn_enemies_over_time,
+                )
+                    .in_set(OnUpdate(AppState::Game))
+                    .in_set(OnUpdate(SimulationState::Running)),
+            );
     }
 }
