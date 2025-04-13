5162c57b8598f3b3d222f16c510047e924c94b6f
xav-ie <github@xav.ie>
2025-04-13 13:41:42 -0400

    feat(pause_menu): do not start the game paused
    This is unconventional and not desired. Start the game running...

--- bevy-ball-game/src/game/ui/pause_menu/mod.rs
+++ bevy-ball-game/src/game/ui/pause_menu/mod.rs
@@ -10,21 +10,26 @@ use systems::{
     layout::{despawn_pause_menu, spawn_pause_menu},
 };
 
-use crate::game::SimulationState;
+use crate::{game::SimulationState, AppState};
 
 pub struct PauseMenuPlugin;
 
 impl Plugin for PauseMenuPlugin {
     fn build(&self, app: &mut App) {
-        app.add_system(spawn_pause_menu.in_schedule(OnEnter(SimulationState::Paused)))
-            .add_system(despawn_pause_menu.in_schedule(OnExit(SimulationState::Paused)))
-            .add_systems(
-                (
-                    interact_with_resume_button,
-                    interact_with_main_menu_button,
-                    interact_with_quit_button,
-                )
-                    .in_set(OnUpdate(SimulationState::Paused)),
-            );
+        app.add_system(
+            spawn_pause_menu
+                .in_schedule(OnEnter(SimulationState::Paused))
+                .run_if(in_state(AppState::Game)),
+        )
+        .add_system(despawn_pause_menu.in_schedule(OnExit(SimulationState::Paused)))
+        .add_system(despawn_pause_menu.in_schedule(OnExit(AppState::Game)))
+        .add_systems(
+            (
+                interact_with_resume_button,
+                interact_with_main_menu_button,
+                interact_with_quit_button,
+            )
+                .in_set(OnUpdate(SimulationState::Paused)),
+        );
     }
 }
